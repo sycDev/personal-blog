c697961ddc2abc87f3f95db74fda7e40eb698e3d
sycDev <sinyi.20028@gmail.com>
2020-02-27 21:42:15 +0800

    Added new files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('user/app')
+
+@section('bg-img', asset('user/img/home-bg.jpg'))
+
+@section('head')
+
+@endsection
+
+@section('title', 'Home')
+
+@section('sub-heading', '')
+
+@section('main-content')
+
+<!-- Post Content -->
+<article>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            
+        </div>
+    </div>
+</div>
+</article>
+
+<hr>
+
+@endsection
+
+@section('footer')
+
+@endsection
